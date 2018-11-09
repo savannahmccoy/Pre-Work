b11a764aea3cd37c4ebf854453db285aa8892957
Savannah McCoy <savannahmccoy82897@Gmail.com>
2018-11-09 17:40:04 -0500

    Initial Commit

---
+++ Pre-Work Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pre-Work Assignment
+//
+//  Created by Savannah McCoy on 11/9/18.
+//  Copyright © 2018 Savannah McCoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
